2529a7b5a219cde877e09d79a584b884c9b8239d
Dominique Legault <dominique@blockchaingroup.io>
2024-06-23 17:41:08 +0000

    chore: better logging of problems

--- packages/components/nodes/agents/OpenAIAssistant/OpenAIAssistant.ts
+++ packages/components/nodes/agents/OpenAIAssistant/OpenAIAssistant.ts
@@ -667,6 +667,7 @@ class OpenAIAssistant_Agents implements INode {
                                         })
                                         resolve(state)
                                     } else {
+                                        console.error('Error submitting tool outputs:', submitToolOutputs, newStatus)
                                         await openai.beta.threads.runs.cancel(threadId, runId)
                                         resolve('requires_action_retry')
                                     }
